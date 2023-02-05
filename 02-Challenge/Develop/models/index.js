db1514f99848eb3d1e374b5cbafafb1f9b164184
Fleaux <hudsonfloyd@yahoo.com>
2023-02-05 00:09:09 -0600

    added the models into the index.js file

--- 02-Challenge/Develop/models/index.js
+++ 02-Challenge/Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  onDelete:'CASCADE'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  onDelete:'CASCADE'
+});
 
 module.exports = {
   Product,
